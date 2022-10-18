db3f0289451d8eb91bca02cd906cc9a50a3e28d2
Petru Giurca <petrugiurca@petrugiurca-2.local>
2022-10-18 16:23:04 +0300

    [Update] Added new remote repo

--- uploadArtifacts.groovy
+++ uploadArtifacts.groovy
@@ -28,7 +28,7 @@ pipeline {
             steps {
                 script {
                     // Let's clone the source
-                    git 'https://github.com/petrugiurca/docker-compose-jenkins-nexus.git';
+                    git 'https://github.com/danielalejandrohc/cargotracker.git';
                 }
             }
         }
